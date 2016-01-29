e82eb6f081622114e94694c2dc10d7fc31675265
kchesterfield <kchesterfield@healthx.com>
2016-01-29 12:54:28 -0500

    Update pi_pong.py
    - GPIO Pin Assignment Updated
    - Winner Label Added
    - Reset GPIO Added

--- pi_pong.py
+++ pi_pong.py
@@ -7,13 +7,15 @@ print("Running Pi-Pong Scoring...")
 import RPi.GPIO as GPIO
 from Tkinter import *
 import tkFont
+import time
 
 # Time before score button can be pressed again (in milleseconds)
-delayTime = 500
+delayTime = 750
 
 # White and blue team GPIO pin assignments (Raspberry Pi B+)
-whitegpio = 18
-bluegpio = 4
+whitegpio = 17
+bluegpio = 27
+redgpio = 22
 
 # Color Assignments
 # The colors listed are HealthX branding colors
@@ -28,7 +30,7 @@ whiteScore = 0
 window = Tk()
 
 # Set font
-labelFont = tkFont.Font(family= 'Arial', size=300, weight='bold')
+labelFont = tkFont.Font(family= 'Arial', size=225, weight='bold')
 exitFont = tkFont.Font(family= 'Arial', size=20)
 
 # Set window parameters
@@ -60,10 +62,34 @@ whiteLabel = Label(window, text="0", font=labelFont, fg=whiteColor, bg='black')
 whiteLabel.pack()
 whiteLabel.place(relx=0.75, rely=0.50, anchor=CENTER)
 
+# Winner Label
+winnerLabel = Label(window, text="", front=labelFont, fg='red', bg='black')
+winnerLabel.pack()
+winnerLabel.place(relx=0.5, rely=0.5, anchor=CENTER)
+
 # GPIO pin setup
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(bluegpio, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(whitegpio, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# Winner display function
+def winnerDisplay(team):
+    global whiteColor
+    global blueColor
+    # Set the score labels to blank values
+    whiteLabel["text"] = ""
+    blueLabel["text"] = ""
+    # Set the winner score label
+    if team == "Blue":
+        winnerLabel.configure(fg=blueColor)
+    elif team == "White":
+        winnerLabel.configure(fg=whiteColor)
+    winnerLabel["text"] = team + " Wins!"
+    whiteLabel.place(relx=0.75, rely=0.50, anchor=CENTER)
+    # Show the label for set amount of time
+    timer.sleep(3)
+    # Set the label back to blank value
+    winnerLabel["text"] = ""
 
 # Blue team score +1 button was hit
 def blue_score(channel):
@@ -75,6 +101,8 @@ def blue_score(channel):
     if blueScore >= 11:
         # Checks to see if score won by atleast 2 points
         if (blueScore - whiteScore) >= 2:
+            # Calls winner function
+            winnerDisplay("Blue")
             # Resets the score to zero for a new round since a winner was found
             blueScore = 0
             whiteScore = 0
@@ -95,6 +123,8 @@ def white_score(channel):
     if whiteScore >= 11:
         # Checks to see if score won by atleast 2 points
         if (whiteScore - blueScore) >= 2:
+            # Calls winner function
+            winnerExists("White")
             # Resets the score to zero for new round since a winner was found
             blueScore = 0
             whiteScore = 0
@@ -105,9 +135,20 @@ def white_score(channel):
     whiteLabel.place(relx=0.75, rely=0.50, anchor=CENTER)
     blueLabel.place(relx=0.25, rely=0.50, anchor=CENTER)
 
+def reset_score(channel):
+    global whiteScore
+    global blueScore
+    # Sets the label text equal to the score
+    whiteLabel["text"] = whiteScore
+    blueLabel["text"] = blueScore
+    # Repositions the label text, this important for when the label changes from single to double digits... example: 9 to 10
+    whiteLabel.place(relx=0.75, rely=0.50, anchor=CENTER)
+    blueLabel.place(relx=0.25, rely=0.50, anchor=CENTER)
+
 # GPIO event detection
 GPIO.add_event_detect(bluegpio, GPIO.FALLING, callback=blue_score, bouncetime=delayTime)
 GPIO.add_event_detect(whitegpio, GPIO.FALLING, callback=white_score, bouncetime=delayTime)
+GPIO.add_event_detect(redgpio, GPIO.FALLING, callback=reset_score, bouncetime=delayTime)
 
 # Begins mainloop
 mainloop()
